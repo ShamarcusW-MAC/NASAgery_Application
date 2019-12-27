555003cb5cf6744167914955fc3b392013fbe7b8
ShamarcusW-MAC <56922234+ShamarcusW-MAC@users.noreply.github.com>
2019-12-27 10:38:04 -0500

    Added in Testing

--- app/src/main/java/com/example/nasagery_application/view/MainActivity.java
+++ app/src/main/java/com/example/nasagery_application/view/MainActivity.java
@@ -10,9 +10,6 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nasagery_application.R;
@@ -31,11 +28,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
     private int add = 20;
-
-    SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
     ImageAdapter imageAdapter;
-    TextView noResultTextView;
 
     private ActivityMainBinding activityMainBinding;
 
@@ -44,13 +38,19 @@ public class MainActivity extends AppCompatActivity {
           super.onCreate(savedInstanceState);
 
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        EditText searchEditText = findViewById(R.id.search_edittext);
-        Button searchButton = findViewById(R.id.search_button);
         nasaViewModel = ViewModelProviders.of(this).get(NASAViewModel.class);
         activityMainBinding.setViewModel(nasaViewModel);
 
-        nasaViewModel.image.observe(this, new Observer<Image>() {
+        activityMainBinding.searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nasaViewModel.makeCall(activityMainBinding.searchEdittext.getText().toString());
+
+            }
+        });
+
+        activityMainBinding.getViewModel().image.observe(this, new Observer<Image>() {
             @Override
             public void onChanged(Image image) {
 
@@ -58,20 +58,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                nasaViewModel.makeCall(searchEditText.getText().toString());
 
-            }
-        });
 
-        swipeRefreshLayout = findViewById(R.id.swipe_recyclerview);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+//      Initiates when the user pulls to refresh the recycler view.
+//      Once initiated, 20 more photos will be added to the view.
+        activityMainBinding.swipeRecyclerview.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                compositeDisposable.add(nasaViewModel.getImage(searchEditText.getText().toString())
+                compositeDisposable.add(nasaViewModel.getImage(activityMainBinding.searchEdittext.getText().toString())
                         .subscribe(images -> {
 
                             {
@@ -79,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
                                 imageAdapter.notifyDataSetChanged();
                                 imageAdapter.limit = add;
                                 add += 20;
-                                swipeRefreshLayout.setRefreshing(false);
+                                activityMainBinding.swipeRecyclerview.setRefreshing(false);
                                 Toast.makeText(MainActivity.this, "Number of photos: " + imageAdapter.getItemCount(), Toast.LENGTH_SHORT).show();
                             }
 
@@ -95,16 +90,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Adapter is initialized and recycler view is filled with items
     private void displayImages(List<Item> images){
         imageAdapter = new ImageAdapter(this, images);
         recyclerView = findViewById(R.id.image_recyclerview);
-        noResultTextView = findViewById(R.id.noresults_textview);
+
         if(images.isEmpty()){
             recyclerView.setVisibility(View.GONE);
-            noResultTextView.setVisibility(View.VISIBLE);
+            activityMainBinding.noresultsTextview.setVisibility(View.VISIBLE);
 
         }else {
-            noResultTextView.setVisibility(View.GONE);
+            activityMainBinding.noresultsTextview.setVisibility(View.GONE);
             recyclerView.setVisibility(View.VISIBLE);
             recyclerView.setAdapter(imageAdapter);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
